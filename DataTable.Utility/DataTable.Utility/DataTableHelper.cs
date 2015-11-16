f6029978a25ac314a091839d62da54bc400f2e86
Gadadhar Singha <singh.gadai@gmail.com>
2015-11-16 13:23:32 +0530

    Command prompt Commit message

---
+++ DataTable.Utility/DataTable.Utility/DataTableHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTableHelper.Utility
+{
+    /// <summary>
+    /// This is static class. the soul purpose of this class is give some extension methods and helper methods for DataTable
+    /// </summary>
+    public static class Utility
+    {
+
+        public static bool Serialize(this DataTable dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
